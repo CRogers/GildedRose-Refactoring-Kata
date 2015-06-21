9577bb2a2f6ed70619672b630c98b0bde9561aa1
Callum Rogers <callumr@palantir.com>
2015-06-21 23:11:24 +0100

    Move everything to test helpers

--- Java/gildedrose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/gildedrose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,121 +1,53 @@
 package com.gildedrose;
 
 import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.*;
-
-import org.hamcrest.FeatureMatcher;
-import org.hamcrest.Matcher;
 import org.junit.Test;
+
+import static com.gildedrose.TestHelpers.FluentGildedRose.gildedRose;
+import static com.gildedrose.TestHelpers.itemNumber;
+import static com.gildedrose.TestHelpers.toSellIn;
+import static com.gildedrose.TestHelpers.withName;
+import static com.gildedrose.TestHelpers.withQuality;
 
 public class GildedRoseTest {
     private final String regularItem = "regular";
 
-    private static class ItemNumber extends FeatureMatcher<GildedRose, Item> {
-        private final int number;
-
-        public ItemNumber(int number, Matcher<Item> subMatcher) {
-            super(subMatcher, "item number " + number, "item's");
-            this.number = number;
-        }
-
-        @Override
-        protected Item featureValueOf(GildedRose actual) {
-            return actual.items[number];
-        }
-    }
-
-    public static Matcher<GildedRose> itemNumber(int number, Matcher<Item> itemMatcher) {
-        return new ItemNumber(number, itemMatcher);
-    }
-
-    private static class ItemName extends FeatureMatcher<Item, String> {
-        public ItemName(Matcher<String> subMatcher) {
-            super(subMatcher, "name", "but name");
-        }
-
-        @Override
-        protected String featureValueOf(Item actual) {
-            return actual.name;
-        }
-    }
-
-    public static Matcher<Item> withName(Matcher<String> nameMatcher) {
-        return new ItemName(nameMatcher);
-    }
-
-    public static Matcher<Item> withName(String name) {
-        return new ItemName(is(name));
-    }
-
-    private static class ItemSellIn extends FeatureMatcher<Item, Integer> {
-        public ItemSellIn(Matcher<Integer> subMatcher) {
-            super(subMatcher, "days to sell in", "but sell in");
-        }
-
-        @Override
-        protected Integer featureValueOf(Item actual) {
-            return actual.sellIn;
-        }
-    }
-
-    public static Matcher<Item> toSellIn(Matcher<Integer> sellingInMatcher) {
-        return new ItemSellIn(sellingInMatcher);
-    }
-
-    public static Matcher<Item> toSellIn(Integer sellingIn) {
-        return new ItemSellIn(is(sellingIn));
-    }
-
-    private static class ItemQuality extends FeatureMatcher<Item, Integer> {
-        public ItemQuality(Matcher<Integer> subMatcher) {
-            super(subMatcher, "quality", "but quality");
-        }
-
-        @Override
-        protected Integer featureValueOf(Item actual) {
-            return actual.quality;
-        }
-    }
-
-    public static Matcher<Item> withQuality(Matcher<Integer> qualityMatcher) {
-        return new ItemQuality(qualityMatcher);
-    }
-
-    public static Matcher<Item> withQuality(Integer quality) {
-        return new ItemQuality(is(quality));
-    }
-
     @Test
     public void regular_item_selling_in_0_with_quality_0() {
-        Item[] items = new Item[] { new Item(regularItem, 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertThat(app, itemNumber(0, allOf(
-            withName(regularItem),
-            toSellIn(-1),
-            withQuality(0))));
+        gildedRose()
+            .startingWith(
+                new Item(regularItem, 0, 0)
+            )
+            .timesUpdatingQuality(1)
+            .shouldMatch(itemNumber(0, allOf(
+                withName(regularItem),
+                toSellIn(-1),
+                withQuality(0))));
     }
 
     @Test
     public void regular_item_selling_in_1_with_quality_0() {
-        Item[] items = new Item[] { new Item(regularItem, 1, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertThat(app, itemNumber(0, allOf(
-            withName(regularItem),
-            toSellIn(0),
-            withQuality(0))));
+        gildedRose()
+            .startingWith(
+                new Item(regularItem, 1, 0)
+            )
+            .timesUpdatingQuality(1)
+            .shouldMatch(itemNumber(0, allOf(
+                withName(regularItem),
+                toSellIn(0),
+                withQuality(0))));
     }
 
     @Test
     public void regular_item_selling_in_1_with_quality_1() {
-        Item[] items = new Item[] { new Item(regularItem, 1, 1) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertThat(app, itemNumber(0, allOf(
-            withName(regularItem),
-            toSellIn(0),
-            withQuality(0))));
+        gildedRose()
+            .startingWith(
+                new Item(regularItem, 1, 1)
+            )
+            .timesUpdatingQuality(1)
+            .shouldMatch(itemNumber(0, allOf(
+                withName(regularItem),
+                toSellIn(0),
+                withQuality(0))));
     }
 }
---
+++ Java/gildedrose/src/test/java/com/gildedrose/TestHelpers.java
@@ -0,0 +1,111 @@
+package com.gildedrose;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import org.hamcrest.FeatureMatcher;
+import org.hamcrest.Matcher;
+
+public class TestHelpers {
+    public static class FluentGildedRose {
+        private Item[] items;
+        private int timesUpdating = 0;
+
+        static FluentGildedRose gildedRose() {
+            return new FluentGildedRose();
+        }
+
+
+        public FluentGildedRose startingWith(Item... items) {
+            this.items = items;
+            return this;
+        }
+
+        public FluentGildedRose timesUpdatingQuality(int i) {
+            this.timesUpdating = i;
+            return this;
+        }
+
+        public void shouldMatch(Matcher<GildedRose> gildedRoseMatcher) {
+            GildedRose app = new GildedRose(items);
+            for (int i = 0; i < timesUpdating; i++) {
+                app.updateQuality();
+            }
+            assertThat(app, gildedRoseMatcher);
+        }
+    }
+
+    static class ItemNumber extends FeatureMatcher<GildedRose, Item> {
+        private final int number;
+
+        public ItemNumber(int number, Matcher<Item> subMatcher) {
+            super(subMatcher, "item number " + number, "item's");
+            this.number = number;
+        }
+
+        @Override
+        protected Item featureValueOf(GildedRose actual) {
+            return actual.items[number];
+        }
+    }
+
+    public static Matcher<GildedRose> itemNumber(int number, Matcher<Item> itemMatcher) {
+        return new ItemNumber(number, itemMatcher);
+    }
+
+    private static class ItemName extends FeatureMatcher<Item, String> {
+        public ItemName(Matcher<String> subMatcher) {
+            super(subMatcher, "name", "but name");
+        }
+
+        @Override
+        protected String featureValueOf(Item actual) {
+            return actual.name;
+        }
+    }
+
+    public static Matcher<Item> withName(Matcher<String> nameMatcher) {
+        return new ItemName(nameMatcher);
+    }
+
+    public static Matcher<Item> withName(String name) {
+        return new ItemName(is(name));
+    }
+
+    private static class ItemSellIn extends FeatureMatcher<Item, Integer> {
+        public ItemSellIn(Matcher<Integer> subMatcher) {
+            super(subMatcher, "days to sell in", "but sell in");
+        }
+
+        @Override
+        protected Integer featureValueOf(Item actual) {
+            return actual.sellIn;
+        }
+    }
+
+    public static Matcher<Item> toSellIn(Matcher<Integer> sellingInMatcher) {
+        return new ItemSellIn(sellingInMatcher);
+    }
+
+    public static Matcher<Item> toSellIn(Integer sellingIn) {
+        return new ItemSellIn(is(sellingIn));
+    }
+
+    private static class ItemQuality extends FeatureMatcher<Item, Integer> {
+        public ItemQuality(Matcher<Integer> subMatcher) {
+            super(subMatcher, "quality", "but quality");
+        }
+
+        @Override
+        protected Integer featureValueOf(Item actual) {
+            return actual.quality;
+        }
+    }
+
+    public static Matcher<Item> withQuality(Matcher<Integer> qualityMatcher) {
+        return new ItemQuality(qualityMatcher);
+    }
+
+    public static Matcher<Item> withQuality(Integer quality) {
+        return new ItemQuality(is(quality));
+    }
+}
