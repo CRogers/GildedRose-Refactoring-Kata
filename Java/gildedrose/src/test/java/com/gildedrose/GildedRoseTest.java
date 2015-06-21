1e47f4f15f5c0a9f2f7e71f5207b6f046c10ae83
Callum Rogers <callumr@palantir.com>
2015-06-21 22:12:37 +0100

    item_foo_selling_in_0_with_quality_0

--- Java/gildedrose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/gildedrose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,5 +1,6 @@
 package com.gildedrose;
 
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
@@ -7,11 +8,13 @@ import org.junit.Test;
 public class GildedRoseTest {
 
     @Test
-    public void foo() {
+    public void item_foo_selling_in_0_with_quality_0() {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertThat(app.items[0].name, is("foo"));
+        assertThat(app.items[0].quality, is(0));
+        assertThat(app.items[0].sellIn, is(-1));
     }
 
 }
