7bcf8be9039bcbc3600590b83b261ac4cc6c438a
Callum Rogers <callumr@palantir.com>
2015-06-21 22:16:59 +0100

    Added another couple of tests

--- Java/gildedrose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/gildedrose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -6,15 +6,35 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class GildedRoseTest {
+    private final String regularItem = "regular";
 
     @Test
-    public void item_foo_selling_in_0_with_quality_0() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    public void regular_item_selling_in_0_with_quality_0() {
+        Item[] items = new Item[] { new Item(regularItem, 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertThat(app.items[0].name, is("foo"));
+        assertThat(app.items[0].name, is(regularItem));
         assertThat(app.items[0].quality, is(0));
         assertThat(app.items[0].sellIn, is(-1));
     }
 
+    @Test
+    public void regular_item_selling_in_1_with_quality_0() {
+        Item[] items = new Item[] { new Item(regularItem, 1, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertThat(app.items[0].name, is(regularItem));
+        assertThat(app.items[0].quality, is(0));
+        assertThat(app.items[0].sellIn, is(0));
+    }
+
+    @Test
+    public void regular_item_selling_in_1_with_quality_1() {
+        Item[] items = new Item[] { new Item(regularItem, 1, 1) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertThat(app.items[0].name, is(regularItem));
+        assertThat(app.items[0].quality, is(0));
+        assertThat(app.items[0].sellIn, is(0));
+    }
 }
